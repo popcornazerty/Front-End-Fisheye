54dd9f169f97ae3e996430b47a79e05726eac3df
popcornazerty <78792807+popcornazerty@users.noreply.github.com>
2021-12-14 15:46:46 +0100

    add pictures + names + css page structure

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,21 @@
 function photographerFactory(data) {
     const { name, portrait } = data;
-
+    const { city, country, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
+    //const description = `assets/photographers/${city}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+       
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        const city = document.createElement('city');
+        city.textContent = city;
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(city);
         return (article);
     }
     return { name, picture, getUserCardDOM }
