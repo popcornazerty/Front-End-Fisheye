bd14070d741ef4731e591950edce15c51998accc
popcornazerty <78792807+popcornazerty@users.noreply.github.com>
2021-12-17 16:50:03 +0100

    add description on each photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,6 +1,6 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-    const { city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price } = data;
+    //const { city, country, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
     //const description = `assets/photographers/${city}`;
 
@@ -9,14 +9,23 @@ function photographerFactory(data) {
        
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
-        const city = document.createElement('city');
-        city.textContent = city;
+        const p = document.createElement('paragraph');
+        const p2 = document.createElement('paragraph2');
+        const p3 = document.createElement('paragraph3');
+        const p4 = document.createElement('paragraph4');
+        p.textContent = city;
+        p2.textContent = country;
+        p3.textContent = tagline;
+        p4.textContent = price;
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(city);
+        article.appendChild(p);
+        article.appendChild(p2);
+        article.appendChild(p3);
+        article.appendChild(p4);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price,  getUserCardDOM }
 }
